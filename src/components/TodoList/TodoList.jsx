1e413f59096d429e0f22d3f9d2f2c0b040694572
hyerimhan <hnhyerim@gmail.com>
2023-01-08 18:38:40 +0900

    feat: Add Todo

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,10 +1,17 @@
 import React, { useState } from "react";
+import AddTodo from "../AddTodo/AddTodo";
 
 export default function TodoList() {
   const [todos, setTodos] = useState([
     { id: "123", text: "장보기", status: "active" },
     { id: "124", text: "공부하기", status: "active" },
   ]);
+
+  const handleAdd = (todo) => {
+    // 새로운 투두를 todos에 업뎃
+    setTodos([...todos, todo]);
+  };
+
   return (
     <section>
       <ul>
@@ -12,6 +19,7 @@ export default function TodoList() {
           <li key={item.id}>{item.text}</li>
         ))}
       </ul>
+      <AddTodo onAdd={handleAdd} />
     </section>
   );
 }
